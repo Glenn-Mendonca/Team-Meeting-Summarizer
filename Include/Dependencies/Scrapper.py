fdfead5053932718bfe990436ac4ec1e9cbc86bd
Glenn Mendonca <glenm.code@gmail.com>
2022-03-31 12:09:02 +0530

    Fixed errors

--- Include/Dependencies/Scrapper.py
+++ Include/Dependencies/Scrapper.py
@@ -1,9 +1,13 @@
 #imports here
 from selenium import webdriver
+import undetected_chromedriver.v2 as uc
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os, time
@@ -16,18 +20,20 @@ class Scrapper():
     # Start Scrapper
     def __init__(self):
         # INIT CHROME OPTIONS
-        chrome_options = Options()
+        chrome_options = webdriver.ChromeOptions()
 
         # SET CHROME OPTIONS
-        chrome_options.add_argument("--use-fake-ui-for-media-stream")
+        chrome_options.add_argument("--use-fake-ui-for-media-stream ")
         chrome_options.add_argument("--disable-notifications")
         chrome_options.add_argument('--log-level=3')
-        chrome_options.add_argument('headless')
-        chrome_options.add_argument("user-agent=User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
+        chrome_options.add_argument('--lang=en')
+        chrome_options.add_argument("--enable-javascript")
+        #chrome_options.add_argument('headless')
+        chrome_options.add_argument("user-agent=User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36")
 
         # Specify the path to chromedriver.exe
         self.driver = webdriver.Chrome(
-            service = Service('D:\Git_Repos\Team_Meetings_Summarizer\WebScrapper\chromedriver.exe'),
+            service = Service('.\Include\Dependencies\chromedriver.exe'),
             options = chrome_options
         )
 
@@ -38,9 +44,7 @@ class Scrapper():
         # Driver URL Path
         self.driver.get(r'https://accounts.google.com/signin/v2/identifier?continue='+\
                          'https://meet.google.com'
-                        )
-
-        #self.login()        
+                        )     
 
     # Google Login
     def login(self, meet_code):
@@ -69,7 +73,7 @@ class Scrapper():
             meetcode.send_keys(self.meetcode)
             joinButton = self.driver.find_element(By.XPATH,'//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div[1]/div[3]/div/div[2]/div[2]/button')
             joinButton.click()
-            self.driver.find_element(By.XPATH,'//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[3]/div/div/div[1]/div[1]/div/div[4]/div[1]/div/div/div')
+            #self.driver.find_element(By.XPATH,'//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[3]/div/div/div[1]/div[1]/div/div[4]/div[1]/div/div/div')
             print("Meet Setup Done...!")
             return self.enterMeet()
         except:
@@ -79,10 +83,11 @@ class Scrapper():
     def enterMeet(self, loop=1):
         try:
             self.driver.implicitly_wait(15)
+            time.sleep(15)
             muteButton = self.driver.find_element(By.XPATH,'//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[3]/div/div/div[1]/div[1]/div/div[4]/div[1]/div/div/div')
-            muteButton.click()
+            muteButton.send_keys(Keys.ENTER)#muteButton.click()
             cameraOff = self.driver.find_element(By.XPATH,'//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[3]/div/div/div[1]/div[1]/div/div[4]/div[2]/div/div')
-            cameraOff.click()
+            cameraOff.send_keys(Keys.ENTER)#cameraOff.click()
             tries = 0
             while(tries<3):
                 try:
@@ -106,6 +111,13 @@ class Scrapper():
     def startCaptions(self):
         try:
             self.driver.implicitly_wait(30)
+            try:
+                captions = self.driver.find_element(By.XPATH, '//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[10]/div[2]/div/div[3]/div/span/button')
+                captions.click()
+                print("Captions turned on...!!")
+                return True
+            except:
+                pass
             options = self.driver.find_element(By.CSS_SELECTOR,'[aria-label="More options"]')
             options.click()
             time.sleep(1.5)
@@ -126,7 +138,7 @@ class Scrapper():
     def extractcaptions(self, senddata):
         speaker, generated_transcript = "", ""
         try:
-            div = self.driver.execute_script('var div = document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.a4cQT > div:nth-child(1)");if (div.parentNode.hasChildNodes()) {html = div.innerHTML;div.parentNode.removeChild(div); return html;} else return "NULL";')
+            div = self.driver.execute_script('var div = document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.a4cQT > div:nth-child(1) > div:nth-child(1) > div");if (div.parentNode.hasChildNodes()) {html = div.innerHTML;div.parentNode.removeChild(div); console.log(html); return html;} else return "NULL";')
             if(div == "NULL"):
                 raise NoSuchElementException
             soup = BeautifulSoup(div,features="html.parser")                     
@@ -137,4 +149,6 @@ class Scrapper():
             senddata({'Speaker':speaker, 'Transcript':generated_transcript})
         except:
             senddata({'Speaker':"", 'Transcript':""})
+        
+
         
